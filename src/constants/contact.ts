ee424c75bae5b25e8812ecb8248c02582d64e5d4
Pablo Navarro <pnavarro0923@gmail.com>
2026-03-29 17:36:10 -0600

    changed email in form

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -1,5 +1,5 @@
 export const WHATSAPP_NUMBER = '50684171560'
-export const CONTACT_EMAIL = 'pnavarro0923@gmail.com'
+export const CONTACT_EMAIL = 'pablo.navarro.eng@gmail.com'
 
 /** Formspree form ID for the contact form. Create a form at https://formspree.io and replace this. */
 export const FORMSPREE_FORM_ID = import.meta.env.VITE_FORMSPREE_FORM_ID || ''
