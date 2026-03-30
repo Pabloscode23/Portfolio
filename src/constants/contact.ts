ab7443b9d5ef6ccefd0af5275a0f5cd853dff171
Pablo Navarro <pnavarro0923@gmail.com>
2026-03-29 18:01:13 -0600

    changed id in env

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -3,3 +3,4 @@ export const CONTACT_EMAIL = 'pablo.navarro.eng@gmail.com'
 
 /** Formspree form ID for the contact form. Create a form at https://formspree.io and replace this. */
 export const FORMSPREE_FORM_ID = import.meta.env.VITE_FORMSPREE_FORM_ID || ''
+
